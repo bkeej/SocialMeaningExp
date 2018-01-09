a98cc2fe4f1ab4fc18f5c07667115598818172ab
Dylan Bumford <1175004+dylnb@users.noreply.github.com>
2018-01-08 23:34:42 -0800

    rerefactor

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,62 @@
 module Main where
 
+import Data.Maybe (fromJust)
+import Lexica
+import ModelTheory
+import LF
+import CompScalar
+import Prob
+import Utils
+import Control.Monad.Trans.Maybe (MaybeT (MaybeT, runMaybeT))
+
 main :: IO ()
-main = return ()
+main = disp_s 1 >> putStrLn "" >> disp_L 1
+-- main = return ()
+
+--
+-- Parameters
+--
+
+data ParamModel m l = PM
+  { worldPrior :: m World
+  , messagePrior :: m (LF l)
+  , lexiconPrior :: m (Lexicon l)
+  }
+
+asModel :: Dist m => ParamModel m ASVocab
+asModel = PM
+  { worldPrior = uniform asUniverse
+  , messagePrior = uniform asMessages
+  , lexiconPrior = uniform asLexica
+  }
+
+asCost :: Cost ASVocab
+asCost Null = 5   -- Only null messages incur asCosts
+asCost _    = 0
+
+--
+-- Testing the model
+--
+
+pretty o mx = dropR 2 probs where
+  probs = "P(.|" ++ show o ++"): " ++ concatMap f (runMassT (runMaybeT mx))
+  f = \(Mass n (Just x)) -> show x ++ " = " ++ prettyN n ++ ", "
+  prettyN (Sum n) = show (fromInteger (round (n * 100)) / 10.0^^2)
+  dropR n xs = fst (splitAt (length xs - n) xs)
+
+disp_s n = sequence_ (map print test)
+  where test = [pretty w (speaker n w asLex1 asCost wp mp)   | w <- asUniverse]
+        wp = worldPrior asModel
+        mp = messagePrior asModel
+
+disp_l n = sequence_ (map print test)
+  where test = [pretty m (listener n m asLex1 asCost wp mp)  | m <- asMessages]
+        wp = worldPrior asModel
+        mp = messagePrior asModel
+
+disp_L n = sequence_ (map print test)
+  where test = [pretty m (lexicaListener n m asCost wp mp lp) | m <- asMessages]
+        wp = worldPrior asModel
+        mp = messagePrior asModel
+        lp = lexiconPrior asModel
+
--- src/CompScalar.hs
+++ src/CompScalar.hs
@@ -19,224 +19,84 @@
 
 module CompScalar where
 
-import           Control.Monad             (guard)
-import           Control.Monad.Trans.Maybe (MaybeT (MaybeT, runMaybeT))
-import           Data.Function             (on)
-import           Data.List                 (groupBy, partition, sortBy)
-import           Data.Maybe                (fromJust)
-import           Prob
-import           Utils
-
---
--- Model-theoretic stuff
---
-
-data Value
-  = VE String
-  | VT Bool
-  | VS World
-  | VF (Value -> Value)
-
-instance Show Value where
-  show (VF f) = "<<fun>>"
-  show (VS w) = "<<world>>"
-  show (VE e) = show e
-  show (VT t) = show t
-
-data World = World
-  { aced'   :: Value
-  , scored' :: Value
-  }
-
-instance Eq World where
-  World (VF ua) (VF us) == World (VF va) (VF vs) =
-    let (VT uaj, VT vaj) = (ua (VE "John"), va (VE "John"))
-        (VT uam, VT vam) = (ua (VE "Mary"), va (VE "Mary"))
-        (VT usj, VT vsj) = (us (VE "John"), vs (VE "John"))
-        (VT usm, VT vsm) = (us (VE "Mary"), vs (VE "Mary"))
-    in uaj == vaj && uam == vam && usj == vsj && usm == vsm
-
-instance Show World where
-  show w | w == wN = "wN"
-         | w == wS = "wS"
-         | w == wA = "wA"
-
-(@@) :: Value -> Value -> Value
-(VF f) @@ x = f x
-
-cup :: Value -> Value -> Value
-cup (VT p) (VT q) = VT (p || q)
-cup (VF f) (VF g) = VF (\x -> f x `cup` g x)
-
-cap :: Value -> Value -> Value
-cap (VT p) (VT q) = VT (p && q)
-cap (VF f) (VF g) = VF (\x -> f x `cap` g x)
-
-neg :: Value -> Value
-neg (VT t) = VT (not t)
-neg (VF f) = VF (\x -> neg (f x))
-
-wA, wS, wN :: World
-wA = World
-  { aced'   = VF (\(VE x) -> VT (x == "John"))
-  , scored' = VF (\(VE x) -> VT (x == "John"))
-  }
-wS = World
-  { aced'   = VF (\(VE x) -> VT False)
-  , scored' = VF (\(VE x) -> VT (x == "John"))
-  }
-wN = World
-  { aced'   = VF (\(VE x) -> VT False)
-  , scored' = VF (\(VE x) -> VT False)
-  }
-
-universe :: [World]
-universe = [wN, wS, wA]
-
---
--- Language stuff
---
-
-data Mess l
-  = Null
-  | Name String
-  | Lex l
-  | Var Int
-  | App (Mess l) (Mess l)
-  | Lam Int (Mess l)
-  deriving (Eq, Show)
-
-data TestLexicon
-  = Some
-  | All
-  | Aced
-  | Scored
-  deriving (Eq, Show)
+import Control.Monad             (guard)
+import Control.Monad.Trans.Maybe (MaybeT (MaybeT, runMaybeT))
+import Data.Maybe                (fromJust)
+import Data.List                 (partition)
+import Data.Function             (on)
+import Prob
+import Utils
+import ModelTheory
+import LF
+import Lexica
 
 type Env = [(Int, Value)]
 
-eval :: Interp l -> Env -> (Mess l) -> Value
+eval :: Lexicon vocab -> Env -> (LF vocab) -> Value
 eval sem env term = case term of
-  Null      -> VF (\w -> VT True)
-  Name e    -> VE e
-  Lex l     -> sem l
-  Var n     -> fromJust $ lookup n env
-  Lam n mb  -> VF (\x -> eval sem ((n,x):env) mb)
+  Null -> VF (\w -> VT True)
+  Name e -> VE (case e of {"John" -> John ; "Mary" -> Mary})
+  Lex l -> sem l
+  Var n -> fromJust $ lookup n env
+  Lam n mb -> VF (\x -> eval sem ((n,x):env) mb)
   App mf mx -> (eval sem env mf) @@ (eval sem env mx)
-
-app :: Mess l -> Mess l -> Mess l
-app = App
-
-lam :: (Mess l -> Mess l) -> Mess l
-lam f = Lam n body
-  where body = f (Var n)
-        n = maxBV body + 1
-        maxBV m = case m of
-          App f x -> maxBV f `max` maxBV x
-          Lam i _ -> i
-          _       -> 0
-
-type Interp l = l -> Value
-
-testI1 :: Interp TestLexicon
-testI1 s = case s of
-  Aced   -> VF (\x -> VF (\(VS w) -> aced'   w @@ x))
-  Scored -> VF (\x -> VF (\(VS w) -> scored' w @@ x))
-
-testI2 :: Interp TestLexicon
-testI2 s = case s of
-  Aced   -> VF $ \x -> VF $ \(VS w) -> aced' w @@ x
-  Scored -> VF $ \x -> VF $ \(VS w) -> (scored' w `cap` (neg $ aced' w)) @@ x
-
-testI3 :: Interp TestLexicon
-testI3 s = case s of
-  Aced   -> VF (\x -> VF (\(VS w) -> aced' w @@ x))
-  Scored -> VF (\x -> VF (\(VS w) -> aced' w @@ x))
-
-testIs ::[Interp TestLexicon]
-testIs = [testI1, testI2, testI3]
-
-testMessages :: [Mess TestLexicon]
-testMessages =
-  [ Lex Aced   `app` Name "John"
-  , Lex Scored `app` Name "John"
-  , Null
-  ]
-
---
--- Model parameters
---
-
-cost Null = 5                         -- Only null messages incur costs
-cost _    = 0                         -- Coerced into Sum's by GHC?
-
-temperature = 1
-
-worldPrior :: Dist m => m World
-worldPrior = uniform universe         -- The primitive notion in Potts et al.
-                                      -- is a prior over *sets* of worlds.
-
-messagePrior :: Dist m => m (Mess TestLexicon)
-messagePrior = uniform testMessages   -- No correspondent in Potts et al. Flat
-                                      -- here so makes no difference.
-
-interpPrior :: Dist m => m (Interp TestLexicon)
-interpPrior = uniform testIs          -- The primitive notion in Potts et al.
-                                      -- is a prior over *sets* of lexica.
 
 --
 -- Arbitrarily pragmatic agents given by mutually recursive functions.
 -- The definitions here are somewhat more general than those of Potts et al.
 --
 
-type Agent l a = Interp l -> BDDist (Mess l) -> BDDist a
+type Cost l = LF l -> Sum Float
+type Agent l a = Lexicon l -> Cost l -> BDDist World -> BDDist (LF l) -> BDDist a
 
-listener :: (Eq l) => Int -> Mess l -> Agent l World
-listener n m sem msgPrior = bayes $ do
-  w <- worldPrior
+listener :: (Eq l) => Int -> LF l -> Agent l World
+listener n m sem cost wrldPrior msgPrior = bayes $ do
+  w <- wrldPrior
   if n <= 0   -- literal listener
     then case (eval sem [] m) @@ (VS w) of VT t -> guard t
     else do   -- pragmatic listener
-      m' <- speaker n w sem msgPrior
+      m' <- speaker n w sem cost wrldPrior msgPrior
       guard (m' == m)
   return w
 
-speaker :: Eq l => Int -> World -> Agent l (Mess l)
-speaker n w sem msgPrior = bayes $ do
+speaker :: Eq l => Int -> World -> Agent l (LF l)
+speaker n w sem cost wrldPrior msgPrior = bayes $ do
   m  <- msgPrior
-  w' <- scaleProb m (listener (n-1) m sem msgPrior)
+  w' <- scaleProb m cost (listener (n-1) m sem cost wrldPrior msgPrior)
   guard (w' == w)
   return m
 
 -- Helper functions for scaling probabilities
-scaleProb :: Mess l -> BDDist a -> BDDist a
-scaleProb m = modify (exp . (temperature *) . subtract (cost m) . log)
+scaleProb :: LF l -> Cost l -> BDDist a -> BDDist a
+scaleProb m cost = modify (exp . (temperature *) . subtract (cost m) . log)
 
 modify :: (Prob -> Prob) -> BDDist a -> BDDist a
 modify f mx = MaybeT (MassT f'd)
   where f'd = [Mass (f n) x | Mass n x <- runMassT (runMaybeT mx)]
 
+temperature = 1
+
 --
 -- Variable-lexica agents
 --
 
-type VLAgent l a = BDDist (Interp l) -> BDDist (Mess l) ->  BDDist a
+type VLAgent l a = Cost l -> BDDist World -> BDDist (LF l) -> BDDist (Lexicon l) -> BDDist a
 
-lexicaSpeaker :: Eq l => Int -> World -> VLAgent l (Mess l)
-lexicaSpeaker n w intPrior msgPrior = bayes $ do
-  m  <- msgPrior
-  w' <- scaleProb m (lexicaListener (n-1) m intPrior msgPrior)
-  guard (w' == w)
-  return m
+lexicaSpeaker :: Eq l => Int -> World -> VLAgent l (LF l)
+lexicaSpeaker n w cost wldPrior msgPrior lexPrior = bayes $ do
+ m  <- msgPrior
+ w' <- scaleProb m cost (lexicaListener (n-1) m cost wldPrior msgPrior lexPrior)
+ guard (w' == w)
+ return m
 
-lexicaListener :: Eq l => Int -> Mess l -> VLAgent l World
-lexicaListener n m intPrior msgPrior = weightedEq . runMassT . bayes $ do
-  w  <- worldPrior
+lexicaListener :: Eq l => Int -> LF l -> VLAgent l World
+lexicaListener n m cost wldPrior msgPrior lexPrior = weightedEq . runMassT . bayes $ do
+  w  <- wldPrior
   m' <- if n <= 1
           then do
-            sem <- intPrior
-            speaker n w sem msgPrior
-          else lexicaSpeaker n w intPrior msgPrior
+            sem <- lexPrior
+            speaker n w sem cost wldPrior msgPrior
+          else lexicaSpeaker n w cost wldPrior msgPrior lexPrior
   guard (m' == m)
   return w
 
@@ -251,22 +111,3 @@ groupEqBy :: (a -> a -> Bool) -> [a] -> [[a]]
 groupEqBy _ [] = []
 groupEqBy f (a:rest) = (a:as) : groupEqBy f bs
   where (as,bs) = partition (f a) rest
-
---
--- Testing the model
---
-
-pretty o mx = dropR 2 probs where
-  probs = "P(.|" ++ show o ++"): " ++ concatMap f (runMassT (runMaybeT mx))
-  f = \(Mass n (Just x)) -> show x ++ " = " ++ prettyN n ++ ", "
-  prettyN (Sum n) = show (fromInteger (round (n * 100)) / 10.0^^2)
-  dropR n xs = fst (splitAt (length xs - n) xs)
-
-disp_s n = sequence_ (map putStrLn test) where
-  test = [pretty w (speaker n w testI1 messagePrior) | w <- universe]
-
-disp_l n = sequence_ (map putStrLn test) where
-  test = [pretty m (listener n m testI1 messagePrior) | m <- testMessages]
-
-disp_L n = sequence_ (map putStrLn test) where
-  test = [pretty m (lexicaListener n m interpPrior messagePrior) | m <- testMessages]
